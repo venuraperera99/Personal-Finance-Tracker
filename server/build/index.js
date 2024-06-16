20ed33df980cdab7edc213509ff209810bc261aa
Venura <venura.perera1999@gmail.com>
2024-06-16 17:14:01 -0400

    Initial commit

---
+++ server/build/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const financial_records_1 = __importDefault(require("./routes/financial-records"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3001;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+const mongoURI = "mongodb+srv://venuraperera1999:DHkl97ii9IvD6S1r@peronsalfinancetracker.nnqbc66.mongodb.net/";
+mongoose_1.default
+    .connect(mongoURI)
+    .then(() => console.log("CONNECTED TO MONGODB!"))
+    .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+app.use("/financial-records", financial_records_1.default);
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+});
---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://venuraperera1999:DHkl97ii9IvD6S1r@peronsalfinancetracker.nnqbc66.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
